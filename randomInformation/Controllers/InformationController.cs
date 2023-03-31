7ac74a25df79a60e7152574fda2828697fb155f1
selcukyazicii <63070617+selcukyazicii@users.noreply.github.com>
2023-03-31 15:29:38 +0300

    results completed

---
+++ randomInformation/Controllers/InformationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace randomInformation.Controllers
+{
+    public class InformationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
